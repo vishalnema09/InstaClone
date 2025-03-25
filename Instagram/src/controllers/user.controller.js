eb55e89bcaae05c03c833c5071ee01fdc8a8f33f
Vishal Nema <vishalnema23@gmail.com>
2025-03-25 15:11:15 +0530

    Add express-validator for user registration input validation

--- Instagram/src/controllers/user.controller.js
+++ Instagram/src/controllers/user.controller.js
@@ -1,6 +1,12 @@
 import userModel from "../models/user.js";
+import { validationResult } from "express-validator";
 
 export const createUserController = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   console.log(req.body);
 
   res.send("register user is done");
--- Instagram/src/routes/user.routes.js
+++ Instagram/src/routes/user.routes.js
@@ -1,7 +1,27 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller.js";
+import { body } from "express-validator";
 const router = Router();
 
-router.post("/register", userController.createUserController);
+router.post(
+  "/register",
+  body("username")
+    .isString()
+    .withMessage("Username must be a string")
+    .isLength({ min: 3, max: 15 })
+    .withMessage("Username must be between 3 and 15 characters long")
+    .custom((value) => value === value.toLowerCase())
+    .withMessage("Username must be lowercase"),
+
+  body("email").isEmail().withMessage("Invalid email address"),
+  
+  body("password")
+    .isString()
+    .withMessage("Password must be a string")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 8 characters long"),
+
+  userController.createUserController
+);
 
 export default router;
