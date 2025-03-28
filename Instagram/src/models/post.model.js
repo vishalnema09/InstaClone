88d0d326750b30f3b44fd851bb97a7f194334382
Vishal Nema <vishalnema23@gmail.com>
2025-03-29 02:59:23 +0530

    Add post model with schema, methods for retrieving and updating posts

---
+++ Instagram/src/models/post.model.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose'
+ 
+ 
+ 
+ const postSchema = new mongoose.Schema({
+     caption: {
+         type: String,
+     },
+     media: {
+         type: String,
+         required: [ true, 'Media is required' ],
+     },
+     author: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'user',
+         required: [ true, 'Author is required' ],
+     },
+ },
+     {
+         timestamps: true,
+     }
+ )
+ 
+ 
+ postSchema.statics.getAuthorPosts = async function (authorId) {
+ 
+     if (!authorId) {
+         throw new Error("Author is required")
+     }
+ 
+     const posts = await this.find({
+         author: authorId
+     })
+ 
+     return posts;
+ 
+ }
+ 
+ postSchema.methods.updateCaption = async function (caption) {
+ 
+     this.caption = caption;
+     await this.save();
+ 
+     return this;
+ }
+ 
+ postSchema.statics.getRecentPosts = async function (limit) {
+ 
+     if (!limit) {
+         throw new Error("Limit is required")
+     }
+ 
+     const posts = await this.find().sort({ createdAt: -1 }).limit(limit);
+ 
+     return posts;
+ 
+ }
+ 
+ const postModel = mongoose.model("post", postSchema);
+ 
+ 
+ export default postModel;
