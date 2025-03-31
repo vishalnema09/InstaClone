4cccce320ef9f7c4d26d6bb48aed5c6c86e4484e
Vishal Nema <vishalnema23@gmail.com>
2025-03-31 11:58:10 +0530

    Add user authentication middleware for Socket.io connections

--- Instagram/src/sockets/socket.io.js
+++ Instagram/src/sockets/socket.io.js
@@ -1,17 +1,42 @@
 import { Server } from "socket.io";
- 
- 
- function initSocket(server) {
- 
-     console.log("Socket.io initialized")
- 
-     const io = new Server(server)
- 
-     io.on("connection", (socket) => {
-         console.log("User connected")
-     })
- 
- }
- 
- 
- export default initSocket;
+import userModel from "../models/user.js";
+
+function initSocket(server) {
+  console.log("Socket.io initialized");
+
+  const io = new Server(server);
+  io.use(async (socket, next) => {
+    try {
+      const token = socket.handshake.headers.token;
+
+      if (!token) {
+        return next(new Error("Token is required"));
+      }
+
+      const decodedToken = userModel.verifyToken(token);
+
+      const user = await userModel.findById(decodedToken._id);
+
+      if (!user) {
+        return next(new Error("User not found"));
+      }
+
+      socket.user = user;
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  io.on("connection", (socket) => {
+    socket.join(socket.user._id.toString());
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+      socket.leave(socket.user._id.toString());
+    });
+    console.log("User connected");
+  });
+}
+
+export default initSocket;
